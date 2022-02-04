c34eb0a78c78fd2a04e24a538f576e2e0bd6fb8d
Adel <adel.amirali@outlook.com>
2022-02-04 12:34:22 +0100

    add DOM elements to main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ window.animate = (function () {
             setTimeout(callback, 1000 / 60);
            }
 })();
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const score = document.getElementById('score');
+canvas.width = 400;
+canvas.height = 400;
